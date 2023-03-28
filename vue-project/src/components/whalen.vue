9bcd23b253fe24c01a4487c38af7b6bb4f83019c
Ishrak24 <Ishrakmiah04@gmail.com>
2023-03-28 12:37:01 -0400

    ++++

---
+++ vue-project/src/components/whalen.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 id="heading">Whalen's in the Closet</h1>
+  <div id="box">
+    <div id="backdrop">
+      <div id="hair"></div>
+      <div id="dress"></div>
+    </div>
+    <div class="buttonBox">
+      <button id="nexthair" onclick="nexthair()">hair➡</button>
+      <button id="nextdress" onclick="nextdress()">dress➡</button>
+    </div>
+  </div>
+</template>
