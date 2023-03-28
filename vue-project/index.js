f8bd2ca709b6cf9ba8f55b0e4e63e31baa7b782a
Ishrak24 <Ishrakmiah04@gmail.com>
2023-03-28 13:14:28 -0400

    +++

---
+++ vue-project/index.js
@@ -0,0 +1,28 @@
+let state = {
+  dress: 0,
+  hair: 0,
+};
+
+nextdress();
+nexthair();
+function nextdress() {
+  let dress = document.querySelector("#dress");
+
+  if (state.dress < 6) {
+    state.dress++;
+    dress.setAttribute("class", `dress${state.dress}`);
+  } else if (state.dress === 6) {
+    state.dress = 0;
+    dress.setAttribute("class", `dress${state.dress}`);
+  }
+}
+function nexthair() {
+  let hair = document.querySelector("#hair");
+  if (state.hair < 7) {
+    state.hair++;
+    hair.setAttribute("class", `hair${state.hair}`);
+  } else if (state.hair === 7) {
+    state.hair = 0;
+    hair.setAttribute("class", `hair${state.hair}`);
+  }
+}
